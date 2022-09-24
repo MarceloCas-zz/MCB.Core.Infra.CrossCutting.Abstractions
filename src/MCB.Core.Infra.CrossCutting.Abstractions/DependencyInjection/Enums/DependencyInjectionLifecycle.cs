ef997cd62b88c55751fb429e23ce978165e03298
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-09-24 00:33:05 -0300

    refact: remove unused IoC code

--- src/MCB.Core.Infra.CrossCutting.Abstractions/DependencyInjection/Enums/DependencyInjectionLifecycle.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MCB.Core.Infra.CrossCutting.Abstractions.DependencyInjection.Enums;
-
-public enum DependencyInjectionLifecycle
-{
-    Transient = 1,
-    Scoped = 2,
-    Singleton = 3
-}
